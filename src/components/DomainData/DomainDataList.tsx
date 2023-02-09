e15c59661790703447baf64ffa93a0f1cc0ab836
Dante Stokes <Dante.Stokes@aggrowth.com>
2023-02-08 22:47:48 -0600

    bug fix

--- src/components/DomainData/DomainDataList.tsx
+++ src/components/DomainData/DomainDataList.tsx
@@ -43,9 +43,9 @@ const DomainDataList = (props: DomainDataShape) => {
     let listItems: JSX.Element[] = [];
     if (subRecords && subRecords.length > 0) { 
         listItems = subRecords.map((record, idx) => {
-            const {registrant: { country, organization, state}, domainName, nameServers} = record;
-            const hostNames = nameServers.hostNames ? nameServers.hostNames : [];
-            const ips = nameServers.ips ? nameServers.ips : [];
+            const {registrant: { country, organization, state}, domainName} = record;
+            const hostNames = record.nameServers ? record.nameServers.hostNames : [];
+            const ips = record.nameServers ? record.nameServers.ips : [];
 
             return <DomainDataListItem 
                         key={`domain-item-${idx + 1}`} 
@@ -58,6 +58,12 @@ const DomainDataList = (props: DomainDataShape) => {
                     />
         })
     }
+    
+    //Check if nameServers was returned as not all domains/ips have this available
+    const nameServers = domainData.nameServers ? domainData.nameServers : null;
+
+    const hostNames = nameServers ? nameServers.hostNames : [];
+    const ips = nameServers ? nameServers.ips : [];
     return (
         <Card>
             {listItems.length > 0 
@@ -67,8 +73,8 @@ const DomainDataList = (props: DomainDataShape) => {
                     organization={domainData.registrant.organization} 
                     state={domainData.registrant.state} 
                     domainName={domainData.domainName} 
-                    hostNames={domainData.nameServers.hostNames} 
-                    ips={domainData.nameServers.ips} 
+                    hostNames={hostNames} 
+                    ips={ips} 
                 />}
         </Card>
     )
